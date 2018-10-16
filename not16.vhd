6331b23423e7aa1e1175874a2e8d8522133db40c
高佳圆 <2016011251@secoder.net>
2018-10-16 18:54:06 +0800

    完善了标志寄存器

--- not16.vhd
+++ not16.vhd
@@ -14,6 +14,7 @@ end not16;
 architecture Behavioral of not16 is
 begin
 	res <= not a;
-	z <= '0';
-	s <= '0';
+	resExt <= res(15 downto 0);
+	z <= not(resExt(15) or resExt(14) or resExt(13) or resExt(12) or resExt(11) or resExt(10) or resExt(9) or resExt(8) or resExt(7) or resExt(6) or resExt(5) or resExt(4) or resExt(3) or resExt(2) or resExt(1) or resExt(0));
+	s <= not resExt(15);
 end Behavioral;
--- xor16.vhd
+++ xor16.vhd
@@ -14,5 +14,6 @@ end xor16;
 architecture Behavioral of xor16 is
 begin
 	res <= a xor b;
-	z <= '0';
+	resExt <= res(15 downto 0);
+	z <= not(resExt(15) or resExt(14) or resExt(13) or resExt(12) or resExt(11) or resExt(10) or resExt(9) or resExt(8) or resExt(7) or resExt(6) or resExt(5) or resExt(4) or resExt(3) or resExt(2) or resExt(1) or resExt(0));
 end Behavioral;
